c874ddef3a439a6e22612514327f054267d7372d
Clyoucker <robbikriper@gmail.com>
2022-12-27 16:07:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/styles/css/style.css";
+import "https://kit.fontawesome.com/cc61d32cd2.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
